c3707fd869fb519335e15547066c7fa271fe74cd
USUARIO <USUARIO@Samuel>
2020-09-27 21:21:27 +0200

    Prac1

--- prog3-battleship/src/mains/MainP1.java
+++ prog3-battleship/src/mains/MainP1.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import model.Coordinate;
 
 public class MainP1 {
-
+	
+	//Programa para probar la clase coordinate
+	
 	public static void main(String[] args) {
+		
+		//Creamos coordenadas
 		
         Coordinate c1 = new Coordinate(0,0);
 	    Coordinate c2 = new Coordinate(10,10);
@@ -15,6 +19,8 @@ public class MainP1 {
 	    Coordinate c5 = new Coordinate(c4);
 	    Coordinate c6 = new Coordinate(2,5);
 
+	    //Imprimimos las coordenadas
+	    
 	    System.out.println(c1);
 	    System.out.println(c2);
 	    System.out.println(c3);
@@ -23,18 +29,26 @@ public class MainP1 {
 	    System.out.println(c6);
 
 
+	    //Imprimimos suma de coordenadas 3 y 6
+	    
         Coordinate sumada = c3.add(c6);
 	    System.out.println(c3+"+"+c6+"="+sumada);
 
+	    //Creamos array de coordenadas
+	    
         Coordinate[] v = new Coordinate[5];
 	    for (int i=0; i<5; i++) {
 	        v[i]= new Coordinate(i,4-i);
 	    }
 	    
+	    //Imprimimos el array de coordenadas
+	    
 	    for (int i=0; i<5; i++) {
 	        System.out.println(v[i].get(0)+","+v[i].get(1));
 	    }
 
+	    //Creamos una lista de coordenadas y lo imprimimos
+	    
 	    ArrayList<Coordinate> v2 = new ArrayList<Coordinate>();
 	    for (int i=0; i<8; i++) {
 	                v2.add(new Coordinate(i, i));
